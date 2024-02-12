fba7a0db126079e3c7bd2332e72a0db400d9b668
Keegan Jamison Welch <114922650+Keegan-Welch@users.noreply.github.com>
2024-02-12 08:23:39 -0500

    create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.io.*;
+import java.swing.*;
+
+public class Main{
+    static boolean running = true;
+    static double = version;
+    static String userName;
+
+    Scanner input = new Scanner(System.in);
+
+    public void windowSetup() {
+        JFrame frame = new JFrame("Meow");
+        JLabel label1 = new JLabel("I love skibidi toilet");
+
+        System.out.printf("Verion: %d", version);
+
+        frame.add(label1);
+
+        frame.setSize(500,500);
+        frame.show();
+    }
+    
+
+    public static void(Main(String args[])) {
+        windowSetup();
+        while (running) {
+
+        }
+    }
+}
