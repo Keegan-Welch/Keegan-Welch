af1a94c22de709cb29a091455d43dea1d948e7fb
Keegan Jamison Welch <114922650+Keegan-Welch@users.noreply.github.com>
2024-02-12 08:30:09 -0500

    create mainLoop

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ import java.swing.*;
 
 public class Main{
     static boolean running = true;
+    static int loops = 0; //times mainLoop has looped
+
     static double = version;
     static String userName;
 
     Scanner input = new Scanner(System.in);
 
+    /**
+     * This sets up the window and shows it.
+     */
     public void windowSetup() {
         JFrame frame = new JFrame("Meow");
         JLabel label1 = new JLabel("I love skibidi toilet");
@@ -20,12 +25,20 @@ public class Main{
         frame.setSize(500,500);
         frame.show();
     }
+
+    /**
+     * This is the main loop that the program runs on.
+     */
+    public void mainLoop() {
+        while (running) {
+            System.out.prinf("loop #%i", loops);
+            loops ++;
+        }
+    }
     
 
     public static void(Main(String args[])) {
         windowSetup();
-        while (running) {
-
-        }
+        mainLoop();
     }
 }
