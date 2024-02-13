3e7dae5dbb1a02bda8630db3675b37575d55e896
Keegan Jamison Welch <114922650+Keegan-Welch@users.noreply.github.com>
2024-02-13 12:04:59 -0500

    initial setup

--- Main.java
+++ Main.java
@@ -1,81 +1,27 @@
 import java.util.Scanner;
-import java.io.*;
-import java.swing.*;
 
-public class Main{
-    static boolean running = true;
-    static int loops = 0; //times mainLoop has looped
+public class Main {
+    public static boolean isRunning = false;
+    public static boolean isLoggedIn = false;
 
-    static String[] names = []{"John Doe", "John Smith", "John Zachary"};
+    public static int loops = 0;
 
-    static double[] versions = []{0.0, 0.1}
-    static double version = 0.1;
+    public static void init() {
 
-    static String userName;
+    }
 
-    
+    public static void mainLoop() {
+        if (isRunning) {
+            while (isRunning) {
 
-    Scanner input = new Scanner(System.in);
-
-    /**
-     * This sets up the window and shows it.
-     */
-    public void windowSetup() {
-        JFrame frame = new JFrame("Meow");
-        JLabel label1 = new JLabel("I love skibidi toilet");
-
-        for (int versionIterator : versions) {
-            if (versionIterator == version) {
-                if (version == 0.0) {
-                    System.out.println("There's nothing here.")
-                } else if (version == 0.1) {
-                    System.out.printf("Verion: %d\n", version);
-
-                    frame.add(label1);
-
-                    frame.setSize(500,500);
-                    frame.isResizeable(false);
-
-                    frame.isAlwaysOnTop(false);
-                    frame.show();
-                } else {
-                    running = false;
-                }
-                
-            } else {
-                System.out.printf("%d is not a valid version.\n", version);
-                running = false;
+                loops += 1;
             }
+        } else {
+            System.out.println("Cannot start mainLoop without initializing")
         }
-        
     }
-
-    /**
-     * This is the main loop that the program runs on.
-     * Each time the loop is complete, a variable called loops is incremented by one point.
-     * If the mainLoop has looped more than 1000 times, the running boolean will be set to false.
-     * The mainLoop will stop.
-     */
-    public void mainLoop() {
-        while (running) {
-            System.out.printf("loop #%i\n", loops);
-
-            for (int name : names) {
-                System.out.println(name);
-            }
-
-            if (loops >= 1000) {
-                loops ++;
-            } else {
-                running = false;
-            }
-    }
-    
-
-    public static void(Main(String args[])) {
-        windowSetup();
-
+    public static void main(String args[]) {
+        init();
         mainLoop();
-        System.out.println("Thank you for using Skibidi");
     }
 }
